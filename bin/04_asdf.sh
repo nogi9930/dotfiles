74c34a7dd29191e29a4787775308cf774f117f9a
nogi9930 <suenaga.9930@gmail.com>
2026-01-24 09:59:13 +0900

    Add development tools.

--- bin/04_asdf.sh
+++ bin/04_asdf.sh
@@ -15,3 +15,13 @@ asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 
 ## Add asdf plugin for Elixir
 asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
+
+## Add asdf plugin for Python
+asdf plugin add python https://github.com/asdf-community/asdf-python.git
+
+### Add asdf plugin for uv tool
+asdf plugin add uv https://github.com/asdf-community/asdf-uv.git
+
+## Add asdf plugin for Flutter
+asdf plugin add flutter https://github.com/asdf-community/asdf-flutter.git
+
